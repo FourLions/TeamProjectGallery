812b0048f90a9e529af7795d72bb5309170fec0b
GeorgeK95 <george_it@abv.bg>
2017-03-14 20:58:48 +0200

    created logout page in UserController.java created profile.html in templates/user created profilePage method in UserController.java

--- gallery/src/main/java/softuniGallery/controller/UserController.java
+++ gallery/src/main/java/softuniGallery/controller/UserController.java
@@ -1,16 +1,26 @@
 package softuniGallery.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import softuniGallery.bindingModel.UserBindingModel;
 import softuniGallery.entity.Role;
 import softuniGallery.entity.User;
 import softuniGallery.repository.RoleRepository;
 import softuniGallery.repository.UserRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class UserController {
@@ -20,11 +30,15 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/register")
-    public String register(Model model) {
-        model.addAttribute("view", "user/register");
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        return "base-layout";
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return "redirect:/login?logout";
     }
 
     @GetMapping("/login")
@@ -33,9 +47,16 @@ public class UserController {
         return "base-layout";
     }
 
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("view", "user/register");
+
+        return "base-layout";
+    }
+
     @PostMapping("/register")
-    public String registerProcess(UserBindingModel userBindingModel){
-        if(!userBindingModel.getPassword().equals(userBindingModel.getConfirmPassword())){
+    public String registerProcess(UserBindingModel userBindingModel) {
+        if (!userBindingModel.getPassword().equals(userBindingModel.getConfirmPassword())) {
             return "redirect:/register";
         }
 
@@ -51,5 +72,18 @@ public class UserController {
 
         this.userRepository.saveAndFlush(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/profile")
+    @PreAuthorize("isAuthenticated()")
+    public String profilePage(Model model) {
+        UserDetails principal = (UserDetails) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        User user = this.userRepository.findByEmail(principal.getUsername());
+        model.addAttribute("user", user);
+        model.addAttribute("view", "user/profile");
+        return "base-layout";
     }
 }
