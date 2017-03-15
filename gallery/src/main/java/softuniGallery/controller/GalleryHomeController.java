cfee3675bf018f0c18e887edc3e103cba776dd3f
Georgi Stalev <georgi_stalev@abv.bg>
2017-03-15 16:36:08 +0200

    4.Article Entity 5.Article-User Relation 6.User-Article Relation 7.Create Article Repository 8.Create Article Controller 9.Creating Articles Part I 10.Creating the View 11.Creating Article Binding Model 12.Creating Articles Part II 13.Add Button to the Base Layout 1.Listing All Articles 2.Single Article Details 1.Creating the Get Method 2.Creating the View 3.Creating the Post Method 4.Hiding Buttons IX.Deleting Articles 1.Creating the Get Method 2.Creating the View 3.Creating the Post Method

--- gallery/src/main/java/softuniGallery/controller/GalleryHomeController.java
+++ gallery/src/main/java/softuniGallery/controller/GalleryHomeController.java
@@ -1,17 +1,28 @@
 package softuniGallery.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import softuniGallery.entity.Article;
+import softuniGallery.repository.ArticleRepository;
 
-/**
- * Created by George-Lenovo on 3/14/2017.
- */
+import java.util.List;
+
 @Controller
 public class GalleryHomeController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
     @GetMapping("/")
     public String index(Model model) {
+
+        List<Article> articles = this.articleRepository.findAll();
+
         model.addAttribute("view", "home/index");
+        model.addAttribute("articles", articles);
+
         return "base-layout";
     }
 }
--- gallery/src/main/java/softuniGallery/entity/User.java
+++ gallery/src/main/java/softuniGallery/entity/User.java
@@ -13,16 +13,27 @@ public class User {
     private String fullName;
     private String password;
     private Set<Role> roles;
+    private Set<Article> articles;
 
     public User(String email, String fullName, String password) {
         this.email = email;
         this.password = password;
         this.fullName = fullName;
         this. roles = new HashSet<>();
+        this.articles = new HashSet<>();
     }
 
     public User() {
 
+    }
+
+    @OneToMany(mappedBy = "author")
+    public Set<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(Set<Article> articles) {
+        this.articles = articles;
     }
 
     @ManyToMany(fetch = FetchType.EAGER)
