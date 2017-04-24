ab41ee9c964c3b002a8f3e1ff28d0a347e7f0882
simeonniko <walkzi@yandex.com>
2017-04-24 12:46:19 +0300

    Added "Remember me" login functionality + my latest commit 265b7b081c183b67daac83cec72f4816ddaaed0c

--- gallery/src/main/java/softuniGallery/config/WebSecurityConfig.java
+++ gallery/src/main/java/softuniGallery/config/WebSecurityConfig.java
@@ -29,16 +29,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().permitAll()
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/admin/**").hasRole("ADMIN")
+                    .anyRequest().permitAll()
                 .and()
-                .formLogin().loginPage("/login")
-                .usernameParameter("email").passwordParameter("password")
+                    .formLogin().loginPage("/login").permitAll()
+                    .usernameParameter("email").passwordParameter("password")
                 .and()
-                .logout().logoutSuccessUrl("/login?logout")
+                    .rememberMe()
+                    .rememberMeCookieName("RememberMe")
+                    .rememberMeParameter("rememberMe")
+                    .key("SecretKey")
+                    .tokenValiditySeconds(100000)
                 .and()
-                .exceptionHandling().accessDeniedPage("/error/403")
+                    .logout().logoutSuccessUrl("/login?logout").permitAll()
                 .and()
-                .csrf();
+                    .exceptionHandling().accessDeniedPage("/error/403")
+                .and()
+                    .csrf();
     }
 }
